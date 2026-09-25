2c43055921c40ad83f5c59e6f7e80791244b5e20
Tamlica <tamlicamuhaimin@outlook.com>
2026-09-25 17:36:48 +0700

    fix: match support email with google branding

--- lib/constants/legal.ts
+++ lib/constants/legal.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = "MakanMap";
 
 // TODO: replace with the address you want shown publicly on /privacy and /terms.
-export const LEGAL_CONTACT_EMAIL = "contact@tamlicamuhaim.in";
+export const LEGAL_CONTACT_EMAIL = "tamlica.muhaimin@gmail.com";
 
 export const LEGAL_LAST_UPDATED = "25 September 2026";
